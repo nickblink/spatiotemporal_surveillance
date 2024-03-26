f587b881c12d89a0162e46fcdcb6e67c16cd9618
Nick Link <nicklink415@gmail.com>
2024-03-26 17:01:32 -0400

    Got WF simulate data down

---
+++ code/functions.R
@@ -0,0 +1,411 @@
+
+#### Helper Functions ####
+### function to add periodic terms to data
+# df: Input data frame.
+# period: period length in months.
+add_periodic_cov <- function(df, period = 12){
+  df = df %>%
+    dplyr::mutate(year = lubridate::year(date) - min(lubridate::year(date)) + 1,
+                  month = month(date),
+                  cos1 = cos(2*1*pi*month/period),
+                  sin1 = sin(2*1*pi*month/period),
+                  cos2 = cos(2*2*pi*month/period),
+                  sin2 = sin(2*2*pi*month/period),
+                  cos3 = cos(2*3*pi*month/period),
+                  sin3 = sin(2*3*pi*month/period))
+  return(df)
+}
+
+### Randomly sample from a quasipoisson. Do this by sampling from a negative binomial with dispersion parameters chosen to emulate the quasipoisson.
+# n: number of samples.
+# mu: mean of the points.
+# theta: dispersion parameter.
+rqpois <- function(n, mu, theta) {
+  rnbinom(n = n, mu = mu, size = mu/(theta-1))
+}
+
+
+#### Simulation Functions ####
+
+### Function to initialize the data frame for data simulation.
+# district_sizes: number of facilities in the districts.
+# start_date: start date for data.
+# end_date: end date for data.
+initialize_df <- function(district_sizes, start_date = '2016-01-01', end_date = '2019-12-01', ...){
+  facilities = unlist(lapply(1:length(district_sizes), function(xx) {paste0(toupper(letters[xx]), sprintf('%03d',1:district_sizes[xx]))}))
+  
+  dates = seq(as.Date(start_date), as.Date(end_date), by = 'month')
+  
+  df = expand.grid(facilities, dates, stringsAsFactors = T)
+  colnames(df) = c('facility','date')
+  
+  df$district = substr(df$facility, 1, 1) 
+  
+  df = df %>%
+    dplyr::select(date, facility, district) %>%
+    arrange(facility, date)
+  return(df)
+}
+
+### Function to sample betas for simulated data.
+# facilities: vector of facility names.
+# b0_mean: mean value b0 is sampled from.
+# b0_sd: standard deviation b0 is sampled from.
+# b1_mean: mean value b1 is sampled from.
+# b1_sd: standard deviation b1 is sampled from.
+sample_betas = function(facilities, b0_mean = 4.3, b0_sd = 1, b1_mean = -0.25, b1_sd = 0.25, ...){
+  betas = matrix(0, nrow = length(facilities), ncol = 8)
+  
+  if(length(b0_mean) == 1){
+    betas[,1] = rnorm(mean = b0_mean, sd = b0_sd, n = nrow(betas))
+  }else{
+    u = sample(b0_mean, nrow(betas), replace = T)
+    betas[, 1] = rnorm(mean = u, sd = b0_sd, n = nrow(betas))
+  }
+  
+  betas[,2] = rnorm(b1_mean, b1_sd, n = nrow(betas))
+  
+  for(j in 3:8){
+    betas[,j] = rnorm(0, 0.15, n = nrow(betas))
+  }
+  
+  # name the rows and columns.
+  rownames(betas) = facilities
+  colnames(betas) = c('intercept', 'year', 'cos1', 'sin1', 'cos2', 'sin2', 'cos3', 'sin3')
+
+  return(betas)
+}
+
+### Function to simulate the data for a variety of situations
+# district_sizes: number of facilities in the districts.
+# R: number of simulated data sets.
+# seed: random seed for reproducibility.
+# type: Model to simulate data. Can be 'WF', 'freqGLM', or 'CAR'.
+# family: Family of simulated data. Can be 'poisson' or 'quasipoisson'.
+simulate_data <- function(district_sizes, R = 1, seed = 10, type = 'WF', family = 'poisson', ...){
+  
+  # set seed so the betas are always the same (the seed input is used later for simulating the data on top of these betas)
+  set.seed(10)
+  
+  # set up data frame
+  df = initialize_df(district_sizes, ...)
+  
+  # make periodic covariates
+  df = add_periodic_cov(df)
+  
+  # get all facility and district names
+  facilities = unique(df$facility)
+  districts = unique(df$district)
+  
+  # get all dates
+  dates = unique(df$date) %>% sort()
+  
+  # sample betas
+  betas = sample_betas(facilities, ...)  
+  
+  if(family == 'poisson'){
+    DGP_function = rpois
+  }else if(family == 'quasipoisson'){
+    DGP_function <- function(n, mu){
+      y <- rqpois(n, mu, theta = list(...)$theta)
+    }
+  }
+  
+  # initialize list of data frames
+  df_lst = list()
+  district_lst = list()
+  
+  # set seed for the data generation
+  set.seed(seed)
+  
+  # simulate the data according to the DGP
+  if(type == 'WF'){
+    # make R sampled sets of data
+    for(i in 1:R){
+      
+      # simulate values given the betas
+      tmp_lst = lapply(facilities, function(xx){
+        tmp = df %>% filter(facility == xx)
+        
+        # keep the 1 for intercepts
+        X = tmp %>% 
+          mutate(intercept = 1) %>%
+          dplyr::select(intercept, year, cos1, sin1, cos2, sin2, cos3, sin3)
+        
+        # error checking
+        if(!identical(colnames(betas), colnames(X))){
+          print(colnamaes(betas))
+          print(colnames(X))
+          stop(sprintf('colnames of betas not equal to X: %s, %s',paste(colnames(betas), collapse = ';'), paste(colnames(X), collapse = ';') ))
+        }
+        
+        # make the 8x1 beta vector for this facility
+        beta_f = t(betas[xx,,drop = F])
+        
+        # get mean prediction from linear model
+        mu = as.matrix(X)%*%beta_f
+        tmp$y_exp = exp(mu)
+        
+        # get Poisson or quasipoisson variance
+        if(family == 'poisson'){
+          tmp$y_var <- tmp$y_exp
+        }else if(family == 'quasipoisson'){
+          tmp$y_var <- list(...)$theta*tmp$y_exp
+        }else{
+          stop('input a proper family')
+        }
+        
+        # simluate random values
+        tmp$y = DGP_function(length(mu), exp(mu))
+        
+        return(tmp)
+      })
+      
+      # combine values into one data frame
+      df = do.call('rbind', tmp_lst)
+      df_lst[[i]] = df
+      
+      # group the results by district
+      district <- df %>%
+        group_by(district, date) %>%
+        summarize(y_exp = sum(y_exp),
+                  y_var = sum(y_var),
+                  y = sum(y),
+                  y_true = sum(y), .groups = 'drop')
+      district_lst[[i]] = district
+      
+    }
+    
+  }else if(type == 'CAR'){
+    
+    rho = list(...)$rho
+    alpha = list(...)$alpha
+    tau2 = list(...)$tau2
+    
+    # add in the mean effects because these are the same for all simulations
+    df = do.call('rbind', lapply(facilities, function(xx){
+      tmp = df %>% filter(facility == xx)
+      
+      # keep the 1 for intercepts
+      X = tmp %>% 
+        mutate(intercept = 1) %>%
+        dplyr::select(intercept, year, cos1, sin1, cos2, sin2, cos3, sin3)
+      
+      # error checking
+      if(!identical(colnames(betas), colnames(X))){
+        browser()
+      }
+      
+      # make the 8x1 beta vector for this facility
+      beta_f = t(betas[xx,,drop = F])
+      
+      # get mean prediction from linear model
+      tmp$mu = (as.matrix(X)%*%beta_f)[,1]
+      
+      return(tmp)
+    }))
+    
+    # make the spatio-temporal precision matrix
+    Q = make_precision_mat(df, rho = rho)
+    
+    tryCatch({
+      V = tau2*solve(Q)
+    }, error = function(e){
+      print(e)
+      print('havent dealt with non-invertible precision matrices yet')
+      browser()
+    })
+    
+    # checking ordering of facilities matches
+    if(!identical(colnames(V), as.character(facilities))){
+      browser()
+      stop('names of covariances and facilities dont match')
+    }
+    
+    # add in the marginal variance to original df
+    dV = diag(V)
+    matid = match(df$facility, names(dV))
+    df$sigma2_marginal = dV[matid]
+    
+    # make R sampled sets of data
+    df_lst = lapply(1:R, function(r){
+      ### get the spatio-temporal random effects
+      # initialize phi
+      phi = matrix(0, nrow = length(dates), ncol = length(facilities))
+      colnames(phi) = facilities
+      
+      # first time step
+      phi[1,] = MASS::mvrnorm(n = 1, mu = rep(0, nrow(V)), Sigma = V)
+      
+      # cycle through other time steps, using auto-correlated priors
+      for(i in 2:length(dates)){
+        phi[i,] = MASS::mvrnorm(n = 1, mu = alpha*phi[i-1,], Sigma = V)
+      }
+      
+      # convert to matching format
+      phi = as.data.frame(phi)
+      phi$date = dates
+      phi_df = tidyr::gather(phi, facility, phi, setdiff(colnames(phi), c('facility','date'))) %>% 
+        arrange(facility, date)
+      
+      # add in the previous phi values
+      phi_df$phi_previous <- c(0,phi_df$phi[1:(nrow(phi_df) - 1)])
+      phi_df$phi_previous[phi_df$date == min(phi_df$date)] <- 0
+      
+      # merge the phi values into the original data frame
+      df = merge(df, phi_df, by = c('date','facility'))
+      
+      # calculate the expected value of the Poisson log-normal
+      df$mu_marginal = df$mu + alpha*df$phi_previous
+      df$y_exp = exp(df$mu_marginal + df$sigma2_marginal/2)
+      
+      # variance from Poisson or quasi-poisson log-normal
+      if(family == 'poisson'){
+        df$y_var = df$y_exp + (exp(df$sigma2_marginal) - 1)*exp(2*df$mu_marginal + df$sigma2_marginal)
+      }else if(family == 'quasipoisson'){
+        df$y_var = list(...)$theta*df$y_exp + (exp(df$sigma2_marginal) - 1)*exp(2*df$mu_marginal + df$sigma2_marginal)
+      }else{
+        stop('input a proper family')
+      }
+      
+      # simulate the observed values
+      df$y = DGP_function(nrow(df), exp(df$mu + df$phi))
+      
+      return(df)
+    })
+    
+    # cycle through each created data frame
+    district_lst = lapply(df_lst, function(df){
+      # cycle through each district
+      district <- do.call('rbind', lapply(districts, function(d){
+        # filter data to this district
+        df2 <- df %>% filter(district == d)
+        facs = unique(df2$facility)
+        
+        # get covariance by this district
+        V_d = V[facs, facs]
+        V_exp = exp(V_d) - 1
+        ind_cov = upper.tri(V_exp) + lower.tri(V_exp)
+        
+        # if(family == 'quasipoisson'){
+        #   warning('cant do district level quasipoisson variance for CAR yet. Havent coded it.')
+        # }
+        
+        district_df = do.call('rbind', lapply(1:length(dates), function(i_date){
+          df3 <- df2 %>% filter(date == dates[i_date])
+          covs = (df3$y_exp%*%t(df3$y_exp))*V_exp
+          cov = sum(covs*ind_cov)
+          
+          tmp_df = data.frame(district = d,
+                              date = dates[i_date],
+                              y_exp = sum(df3$y_exp),
+                              y_var_ind = sum(df3$y_var),
+                              y_cov = cov,
+                              y_var = sum(df3$y_var) + cov,
+                              y = sum(df3$y),
+                              y_true = sum(df3$y))
+          return(tmp_df)
+        }
+        ))
+        return(district_df)
+      }))
+      return(district)
+    })
+    
+  }else if(type == 'freqGLM'){
+    rho = list(...)$rho
+    alpha = list(...)$alpha
+    
+    # get the adjacency matrix
+    W <- make_district_adjacency(df, scale_by_num_neighbors = T)
+    
+    # add in the mean effects because these are the same for all simulations
+    df = do.call('rbind', lapply(facilities, function(xx){
+      tmp = df %>% filter(facility == xx)
+      
+      # keep the 1 for intercepts
+      X = tmp %>% 
+        mutate(intercept = 1) %>%
+        dplyr::select(intercept, year, cos1, sin1, cos2, sin2, cos3, sin3)
+      
+      # error checking
+      if(!identical(colnames(betas), colnames(X))){
+        browser()
+      }
+      
+      # make the 8x1 beta vector for this facility
+      beta_f = t(betas[xx,,drop = F])
+      
+      # get mean prediction from linear model
+      tmp$mu = (as.matrix(X)%*%beta_f)[,1]
+      
+      return(tmp)
+    }))
+    
+    df$y_exp = df$y_var = df$y = NA
+    
+    # make R sampled sets of data
+    df_lst = lapply(1:R, function(r){
+      
+      ### Get the first time point values
+      ind = which(df$date == min(dates))
+      
+      # the adjustment accounts for the fact that there aren't additive auto-regressive and spatial terms at the first time point.
+      # this adjustment comes from the sum of a geometric series (since this is roughly the effect that the spatial and autoregressive terms approach as we increase the time series)
+      adjustment = 1 + rho/(1-rho) + alpha/(1-alpha)
+      df$y_exp[ind] = df$y_var[ind] = adjustment*exp(df$mu[ind])
+      df$y[ind] = DGP_function(length(ind), df$y_exp[ind])
+      
+      # update the neighbors and auto-regressive terms
+      df <- add_autoregressive(df, 'y') %>%
+        add_neighbors(., 'y', scale_by_num_neighbors = T, W = W)
+      
+      ### remaining time points
+      for(d in dates[-1]){
+        # get the subset of dates
+        ind = which(df$date == d)
+        
+        # get the mean estimates for these dates
+        df$y_exp[ind] <- exp(df$mu[ind]) + 
+          alpha*df$y.AR1[ind] + rho*df$y.neighbors[ind]
+        
+        # get Poisson or quasipoisson variance
+        if(family == 'poisson'){
+          df$y_var[ind] <-df$y_exp[ind]
+        }else if(family == 'quasipoisson'){
+          df$y_var[ind] <- list(...)$theta*df$y_exp[ind]
+        }else{
+          stop('input a proper family')
+        }
+        
+        # predict!
+        df$y[ind] = DGP_function(length(ind), df$y_exp[ind])
+        
+        # update the neighbors and auto-regressive terms
+        df <- add_autoregressive(df, 'y') %>%
+          add_neighbors(., 'y', scale_by_num_neighbors = T, W = W)
+      }
+      # ggplot(df, aes(x = date, y = y)) + 
+      #   geom_line() + 
+      #   facet_wrap(~facility)
+      df
+    })
+    
+    # group the results by district
+    district_lst <- lapply(df_lst, function(df){
+      district <- df %>%
+        group_by(district, date) %>%
+        summarize(y_exp = sum(y_exp),
+                  y_var = sum(y_var),
+                  y = sum(y),
+                  y_true = sum(y))
+      district
+    })
+  }else{
+    stop('please input a proper type')
+  }
+  
+  # make list of values to return
+  res_lst = list(df_list = df_lst, district_list = district_lst, betas = betas)
+  return(res_lst)
+}
---
+++ code/main.R
@@ -0,0 +1,237 @@
+library(MASS)
+#library(CARBayesST)
+library(Matrix)
+library(dplyr)
+library(lubridate)
+library(ggplot2)
+library(rstan)
+
+source('code/functions.R')
+rstan_options(auto_write = TRUE)
+
+# get the parameters, separated by ":".
+# For submission to a high performance computing cluster, this allows parameters to be input with `inputs <- commandArgs(trailingOnly = TRUE)`
+inputs <- c('p=0.1:b0_mean=6:b1_mean=n0.25:missingness=mcar:DGP=WF:R=1000:num_jobs=50:output_path=mcar01_WF_QPtheta9_beta06_beta1n025_ID499135_2023_12_05:theta=9:family=quasipoisson\r','3')
+
+### Get parameters
+{
+# pull parameters into proper format
+params <- list()
+
+inputs[[1]] <- gsub('\r', '', inputs[[1]])
+params[['job_id']] <- as.integer(inputs[[2]])
+for(str in strsplit(inputs[[1]],':')[[1]]){
+  tmp = strsplit(str, '=')[[1]]
+  nn = tmp[1]
+  val = tolower(tmp[2])
+  if(nn %in% c('p','rho_DGP','alpha_DGP','tau2_DGP','rho_MAR','alpha_MAR','tau2_MAR','gamma','theta')){
+    val = as.numeric(val)
+  }else if(nn == 'b0_mean'){
+    val = as.numeric(strsplit(val, '/')[[1]])
+  }else if(nn == 'b1_mean'){
+    val = tryCatch({as.numeric(val)
+    }, warning = function(w){
+      if(substr(val,1,1) == 'n'){
+        return(-as.numeric(substr(val, 2, nchar(val))))
+      }
+    })
+  }else if(nn %in% c('R','num_jobs')){
+    val = as.integer(val)
+  }
+  params[[nn]] = val
+}
+
+# check that proper missingness is input
+if(!(params[['missingness']] %in% c('mcar','mar','mnar'))){
+  stop('please input a proper missingness')
+}else{
+  print(sprintf('proceeding with %s missingness', params[['missingness']]))
+}
+
+# get sequence of simulation iterations to run
+# (deprecated - now I just simulate R_new # of data frames)
+if(params[['job_id']] < params[['num_jobs']]){
+  seq <- (floor(params[['R']]/params[['num_jobs']])*(params[['job_id']] - 1) + 1):(floor(params[['R']]/params[['num_jobs']])*params[['job_id']])
+}else{
+  seq <- (floor(params[['R']]/params[['num_jobs']])*(params[['job_id']] - 1) + 1):params[['R']]
+}
+
+
+R_new = length(seq)
+
+# input arguments to data simulation
+arguments = list(district_sizes = c(4, 6, 10), 
+                 R = R_new, 
+                 seed = params[['job_id']],
+                 end_date = '2020-12-01', 
+                 b0_mean = params[['b0_mean']], 
+                 b1_mean = params[['b1_mean']])
+
+if(params[['DGP']] == 'car'){
+  arguments = c(arguments, 
+                   list(type = 'CAR',
+                   rho = params[['rho_DGP']], 
+                   alpha = params[['alpha_DGP']], 
+                   tau2 = params[['tau2_DGP']]))
+}else if(params[['DGP']] == 'freqglm'){
+  arguments = c(arguments,
+                list(type = 'freqGLM',
+                     rho = params[['rho_DGP']],  
+                     alpha = params[['alpha_DGP']]))
+}
+
+if(!is.null(params[['family']])){
+  if(params[['family']] == 'quasipoisson'){
+    arguments = c(arguments,
+                  list(family = 'quasipoisson',
+                       theta = params[['theta']]))
+  }else if(params[['family']] != 'poisson'){
+    stop('improper family for DGP')
+  }
+}
+
+# Simulate the data
+lst <- do.call(simulate_data, arguments)
+
+# initialize the error catcher for each run
+errors <- list(freqGLM = data.frame(i = NULL, error = NULL),
+               WF = data.frame(i = NULL, error = NULL),
+               CAR = data.frame(i = NULL, error = NULL))
+
+}
+
+SO FAR GOT THIS WORKING FOR WF DGP, but havent gotten freqGLM or CAR yet.
+
+### File saving (for cluster only)
+{
+# set up the output folder
+date <- gsub('-','_', Sys.Date())
+if(params[['output_path']] == 'na'){
+  params[['output_path']] <- sprintf('results/%s%s_%s_%s', params[['missingness']], gsub('\\.','', params[['p']]), params[['DGP']], date)
+}else{
+  params[['output_path']] <- sprintf('results/%s', params[['output_path']])
+}
+
+if(!file.exists(params[['output_path']])){
+  dir.create(params[['output_path']], recursive = T)
+}
+results_file <- sprintf('%s/sim_results_p%1.1f_%s_%i(%i).RData', params[['output_path']], params[['p']], params[['missingness']], params[['job_id']], params[['num_jobs']])
+
+if(file.exists(results_file)){
+  iter = 0
+  while(file.exists(results_file)){
+    iter = iter + 1
+    results_file <- sprintf('%s/sim_results_p%1.1f_%s_%i(%i)(%i).RData', params[['output_path']], params[['p']], params[['missingness']], params[['job_id']], params[['num_jobs']], iter)
+  }
+}
+
+# save the data
+if(params[['job_id']] == 1){
+  save(lst, file = paste0(params[['output_path']], '/simulated_data.RData'))
+}
+
+}
+
+# function to run all models for a specific dataset
+one_run <- function(lst, i, models = c('freq', 'WF', 'CAR'), WF_params = list(R_PI = 200), freqGLM_params = list(R_PI = 200), MCMC_params = list(burnin.stan = 1000, n.sample.stan = 2000, burnin.CARBayesST = 5000, n.sample.CARBayesST = 10000)){
+  
+  set.seed(i)
+  
+  t0 <- Sys.time()
+  print(sprintf('i = %i',i))
+  df = lst$df_list[[i]]
+  
+  set.seed(i)
+   # add in missingness
+  if(params[['missingness']] == 'mcar'){
+    df_miss = MCAR_sim(df, p = params[['p']], by_facility = T)
+  }else if(params[['missingness']] == 'mar'){
+    df_miss = MAR_spatiotemporal_sim(df, p = params[['p']], rho = params[['rho_MAR']], alpha = params[['alpha_MAR']], tau2 = params[['tau2_MAR']])
+  }else{
+    df_miss <- MNAR_sim(df, p = params[['p']], direction = 'upper', gamma = params[['gamma']], by_facility = T)
+  }
+  
+  # initializing the return list
+  return_list <- list(df_miss = df_miss, district_df = lst$district_list[[i]], errors = errors, timing = list())
+  rm(df_miss)
+  
+  set.seed(i)
+  # run the freqGLM_epi complete case analysis
+  if('freq' %in% models){
+    t1 = Sys.time()
+    print('running freqGLM_epi')
+    return_list <- tryCatch({
+      freqGLMepi_list = freqGLMepi_CCA(return_list[['df_miss']], R_PI = freqGLM_params[['R_PI']], verbose = F)
+      return_list[['df_miss']] <- freqGLMepi_list$df
+      return_list[['district_df']] <- merge(return_list[['district_df']], freqGLMepi_list$district_df, by = c('district','date'))
+      return_list[['freqGLM_params']] <- freqGLMepi_list$param_results
+      return_list
+    }, error = function(e){
+      return_list[['errors']][['freqGLM']] <- rbind(return_list[['errors']][['freqGLM']], data.frame(i, error = e[[1]]))
+      return_list
+    })
+    return_list[['timing']][['freq']] <- as.numeric(difftime(Sys.time(), t1, units = 'm'))
+  }
+  
+  set.seed(i)
+  # run the WF complete case analysis model
+  if('WF' %in% models){
+    t1 = Sys.time()
+    print('running WF CCA')
+    return_list <- tryCatch({
+      res <- WF_CCA(return_list[['df_miss']], col = "y", family = 'poisson', R_PI = WF_params[['R_PI']])
+      return_list[['df_miss']] <- res$df
+      return_list[['district_df']] <- merge(return_list[['district_df']], res$district_df, by = c('district','date'))
+        res$district_df
+      return_list[['WF_betas']] <- res$betas
+      return_list
+    }, error = function(e){
+      return_list[['errors']][['WF']] <- rbind(return_list[['errors']][['WF']], data.frame(i = i, error = e[[1]]))
+      return_list
+    })
+    return_list[['timing']][['WF']] <- as.numeric(difftime(Sys.time(), t1, units = 'm'))
+  }
+
+  set.seed(i)
+  if('CAR' %in% models){
+    t1 = Sys.time()
+    print('running CARBayes with stan')
+    return_list <- tryCatch({
+      res <- CARBayes_wrapper(return_list[['df_miss']], burnin = MCMC_params[['burnin.stan']], n.sample = MCMC_params[['n.sample.stan']], prediction_sample = T, model = 'facility_fixed', predict_start_date = '2016-01-01', MCMC_sampler = 'stan')
+      
+      # rename the columns
+      colnames(res$df) <- gsub('y_pred_CAR', 'y_CARstan', colnames(res$df))
+      colnames(res$district_df) <- gsub('y_pred_CAR', 'y_CARstan', colnames(res$district_df))
+      # update the results list
+      return_list[['df_miss']] <- res$df
+      return_list[['district_df']] <- merge(return_list[['district_df']], res$district_df, by = c('district','date'))
+      return_list[['CARstan_summary']] <- res$CARstan_summary
+      return_list
+    }, error = function(e){
+      print(e)
+      return_list[['errors']][['CAR']] <- rbind(return_list[['errors']][['CAR']], data.frame(i, error = e[[1]]))
+      return_list
+    })
+    return_list[['timing']][['CAR']] <- as.numeric(difftime(Sys.time(), t1, units = 'm'))
+  }
+
+  print(sprintf('i = %i; time = %f minutes', i, difftime(Sys.time(), t0, units = 'm')))
+  
+  # check I got the correct result names
+  # outcome_name_checker(return_list, models = models)
+  
+  return(return_list)
+}
+
+# run the models for each simulation dataset
+system.time({
+  #imputed_list <- foreach(i=seq) %dorng% one_run(lst, i, models = c('freq', 'WF', 'CARstan'))
+  imputed_list <- foreach(i=1:R_new) %dorng% one_run(lst, i, models = c('freq', 'WF', 'CARstan'))
+})
+
+# res <- one_run(lst, 1, freqGLM_params = list(R_PI = 5), MCMC_params = list(burnin.stan = 20, n.sample.stan = 50, burnin.CARBayesST = 100, n.sample.CARBayesST = 200))
+
+true_betas <- lst$betas
+
+save(imputed_list, seq, params, arguments, true_betas, file = results_file)
+
